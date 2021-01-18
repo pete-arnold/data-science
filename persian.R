b8827e1a5298b5b984fa1117584ab4ee0d795957
pete.arnold <pete.arnold@chi.swan.ac.uk>
2021-01-18 14:15:14 +0000

    Modified the comment.

--- persian.R
+++ persian.R
@@ -1,6 +1,7 @@
 # ------------------------------------------------------------------------------
 # Matrices and contours
 # Persian Rug Art
+# Pete's Changes
 # ------------------------------------------------------------------------------
 # outer is also possible as %o% compared to inner product %*%.
 # also have kronecker
