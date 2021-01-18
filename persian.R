dcf05e0e2fc8240578f4296f07680c0c25756e56
shashidhar <shashidhar.ameenpur@gmail.com>
2021-01-18 19:54:31 +0530

    shashidhar change

--- persian.R
+++ persian.R
@@ -1,6 +1,7 @@
 # ------------------------------------------------------------------------------
 # Matrices and contours
 # Persian Rug Art
+#Made changes
 # ------------------------------------------------------------------------------
 # outer is also possible as %o% compared to inner product %*%.
 # also have kronecker
