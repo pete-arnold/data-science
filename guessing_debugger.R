2e72578700b9c34dcf44f6eb2a9251611b98c4aa
Pete Arnold <p.t.arnold@swansea.ac.uk>
2021-10-28 09:17:24 +0100

    Guessing game with function def.

--- guessing_debugger.R
+++ guessing_debugger.R
@@ -5,6 +5,35 @@
 # 26 October 2021
 # ------------------------------------------------------------------------------
 
+# A function definition contains:
+# -------------------------------
+# The word 'function()' to indicate that it is a function.
+# The brackets contain a list of parameter names.
+# These names can then be referred to in the function code.
+# When the function is used in your code, you supply values for these parameters.
+# In your definition, you can provide a default value (using '=').
+# The function code is contained in the curly brackets, { <code> }.
+# When the function is used, it does it's work which may produce a value to be
+#    returned. This value is returned using return(value). The part of your
+#    code that uses the function effectively becomes this value. For example,
+#    x <- guessing_game(5)
+#    will run the game below with a parameter, 5, which will be loaded into
+#    max_attempts (just for this particular use). When the function finishes,
+#    the return(list(values)) line means that the original code,
+#    x <- guessing_game(5) is now, effectively
+#    x <- list(values)
+#    and the list of values from the guessing_game() function are stored in a
+#    variable, x.
+# You could use the function as:
+#    x <- guessing_game(max_attempts=8)  # if you want to name the parameter
+#                                        # to make the code clearer
+#    n <- 16
+#    x <- guessing_game(n)               # if you want to vary the number
+#    x <- guessing_game()                # to use the default number specified in
+#                                        # the definition (i.e. 10).
+#    x <- guessing_game(runif(1, 10, 20))# to set a random number for the max
+#                                        # attempts.
+
 # The guessing game as a function.
 # See the guessing game exercises from day 1.
 # @param max_attempts integer The maximum number of attempts you have to guess.
@@ -43,13 +72,13 @@ guessing_game <- function(max_attempts=10){
             }
             number <- number + round(runif(1, -change, change), 0)
             numbers <- c(numbers, number)
-        } 
+        }
     }
     print("You guessed the following values:")
     print(guesses)
     plot(guesses, type='l', col='red')
     lines(numbers, col='blue')
-    
+
     return(list(nums=numbers, attempts=guesses))
 }
 
